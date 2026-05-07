da63560933d7aac9f095c1d55173717c257eebc1
Tetsu <ttgchang@gmail.com>
2026-05-07 17:23:21 +0800

    更新BASE_URL

--- homework.js
+++ homework.js
@@ -9,7 +9,7 @@ require("dotenv").config({ path: ".env" });
 
 // API 設定（從 .env 讀取）
 const API_PATH = process.env.API_PATH;
-const BASE_URL = process.env.API_BASE;
+const BASE_URL = "https://livejs-api.hexschool.io/";
 const ADMIN_TOKEN = process.env.API_KEY;
 
 // ========================================
